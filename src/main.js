d63abbcb6ee2a794fd7077f14e3e6b4baf2da3a9
Patrick Burke <denormative@gmail.com>
2017-05-01 13:00:18 +0900

    add papaparse for csv

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Papa from 'papaparse' // eslint-disable-line
 import App from './App'
 import router from './router'
 
